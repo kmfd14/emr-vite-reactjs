cfc61175457aceecd91d9455c19db22b0dbbcb22
Kristoffer Matthew Decena <kristoffer.decena@jeonsoft.com>
2024-07-05 13:09:18 +0800

    fix sidebar profile dropdown to open upwards

--- src/assets/fontawesome.js
+++ src/assets/fontawesome.js
@@ -1,7 +1,7 @@
 // src/fontAwesome.js
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faHome, faUser, faCity } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faHome, faUser, faCity, faBars } from '@fortawesome/free-solid-svg-icons'
 import { faSmile } from '@fortawesome/free-regular-svg-icons'
 import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faCoffee, faHome, faUser, faSmile, faFacebook, faTwitter, faCity)
+library.add(faCoffee, faHome, faUser, faSmile, faFacebook, faTwitter, faCity, faBars)
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faHome, faCity } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   const [activeLink, setActiveLink] = useState('/')
   const [isMinimized, setIsMinimized] = useState(false)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const dropdownRef = useRef(null) // Reference to the dropdown element
 
   const handleLinkClick = (path) => {
     setActiveLink(path)
@@ -13,7 +14,29 @@ const Sidebar = () => {
 
   const toggleSidebar = () => {
     setIsMinimized(!isMinimized)
+    if (!isMinimized) {
+      setDropdownOpen(false)
+    }
   }
+
+  const toggleDropdown = (e) => {
+    e.preventDefault()
+    setDropdownOpen(!dropdownOpen)
+  }
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false)
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside)
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
 
   return (
     <>
@@ -25,8 +48,8 @@ const Sidebar = () => {
               <span className="fs-4">Sidebar</span>
             </a>
           )}
-          <button className="btn btn-dark" onClick={toggleSidebar}>
-            <FontAwesomeIcon icon={faBars} />
+          <button className={`btn btn-dark ${isMinimized ? 'm-auto' : ''}`} onClick={toggleSidebar}>
+            <FontAwesomeIcon icon='fa fa-bars' />
           </button>
         </div>
         <hr />
@@ -37,7 +60,7 @@ const Sidebar = () => {
               className={`nav-link ${activeLink === '/' ? 'active' : ''} text-white`}
               onClick={() => handleLinkClick('/')}
             >
-              <FontAwesomeIcon icon={faHome} /> {!isMinimized && 'Home'}
+              <FontAwesomeIcon icon='fa fa-home' /> {!isMinimized && 'Home'}
             </Link>
           </li>
           <li className="nav-item">
@@ -46,27 +69,32 @@ const Sidebar = () => {
               className={`nav-link ${activeLink === '/about2' ? 'active' : ''} text-white`}
               onClick={() => handleLinkClick('/about2')}
             >
-              <FontAwesomeIcon icon={faCity} /> {!isMinimized && 'About'}
+              <FontAwesomeIcon icon='fa fa-city' /> {!isMinimized && 'About'}
             </Link>
           </li>
           {/* Other menu items */}
         </ul>
         <hr />
-        {!isMinimized && (
-          <div className="dropdown">
-            <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
-              <strong>mdo</strong>
-            </a>
-            <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-              <li><a className="dropdown-item" href="#">New project...</a></li>
-              <li><a className="dropdown-item" href="#">Settings</a></li>
-              <li><a className="dropdown-item" href="#">Profile</a></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="#">Sign out</a></li>
-            </ul>
-          </div>
-        )}
+        <div className={`dropup ${dropdownOpen && 'show'}`} ref={dropdownRef}>
+          <a 
+            href='#' 
+            className={`d-flex align-items-center text-white text-decoration-none ${!isMinimized ? 'dropdown-toggle' : ''}`} 
+            id="dropdownUser1" 
+            onClick={isMinimized ? toggleDropdown : undefined}
+            data-bs-toggle={!isMinimized ? 'dropdown' : undefined} 
+            aria-expanded={dropdownOpen}
+          >
+            <img src="https://github.com/mdo.png" alt="User Icon" width="32" height="32" className={`rounded-circle ${isMinimized ? 'm-auto' : 'me-2'}`} />
+            {!isMinimized && <strong>mdo</strong>}
+          </a>
+          <ul className={`dropdown-menu dropdown-menu-dark text-small shadow ${dropdownOpen && 'show'}`} aria-labelledby="dropdownUser1" data-bs-popper={isMinimized ? 'static' : undefined}>
+            <li><a className="dropdown-item" href="#">New project...</a></li>
+            <li><a className="dropdown-item" href="#">Settings</a></li>
+            <li><a className="dropdown-item" href="#">Profile</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
       </div>
     </>
   )
